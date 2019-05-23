7bb6a9b36bfd224758ae9b16b4d5b37c616c17bf
reemaamhaz <49565663+reemaamhaz@users.noreply.github.com>
2019-05-23 14:07:37 -0700

    Update appTest.js

--- appTest.js
+++ appTest.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const app = require('../app');
-
 
 processAllEmployeesResult = app.processAllEmployees();
 splitFullNameResult = app.splitFullName();
@@ -8,20 +8,24 @@ printOutFirstNamesResult = app.printOutFirstNames();
 
 describe('app', function(){
     describe('processAllEmployees()', function(){
-        it('should process all the employees', function(){ 
-            assert.isArray(processAllEmployeesResult, 'Creed')
+        it('should process the employees and populate namesLeft', function(){ 
+            expect(processAllEmployeesResult).to.include('Kelly Kapoor');
         });
     });
 
     describe('splitFullName()', function(){
         it('should split the names', function(){
-            assert.isArray(splitFullNameResult, 'Kelly')
+            expect(splitFullNameResult).to.include('Creed');
+        });
+
+        it('return first names only', function(){
+            expect(splitFullNameResult).to.not.include('Bratton');
         });
     });
 
     describe('printOutFirstNames()', function(){
         it('should print out the first names', function(){
-            assert.isString(printOutFirstNamesResult, 'Pam')
+            assert.equal(printOutFirstNamesResult, 'Clark')
         });
     });
 });
